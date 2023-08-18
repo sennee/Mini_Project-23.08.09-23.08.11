e3614fb9ecf20abb5c3f47a3f2b38c85a2c0af66
sennee <sennee1029@gmail.com>
2023-08-18 15:48:16 +0900

    Commit!!

---
+++ miniProject4/src/main/java/com/example/miniProject4/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.miniProject4;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:///D:/BOOT CAMP/Mini_project (0809~0811)/item_images/");
+    }
+}
